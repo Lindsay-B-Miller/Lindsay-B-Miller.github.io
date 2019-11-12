f5387571e4d49933d0b3a660512c8dfa29ba1d35
Lindsay <lindsay.lbm@gmail.com>
2019-11-11 18:48:39 -0600

    navbar gray fades in with scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ $(window).on('scroll', function (event) {
     var scrollValue = $(window).scrollTop();
     if (scrollValue > 320) {
         $('.navbar').addClass('affix');
+        $(".navbar").addClass("animated fadeIn")
     } else {
         $('.navbar').removeClass('affix');
     }
