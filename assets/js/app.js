2e1964a4740353a1cbb2862ce8afc624ccee65ed
Lindsay <lindsay.lbm@gmail.com>
2019-11-11 19:09:04 -0600

    added github and linkedin icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,9 @@
-// /* affix the navbar after scroll below header */
-// $('#nav').affix({
-//     offset: {
-//         top: $('#services').offset().top
-//     }
-// });
-$(window).on('scroll', function (event) {
+$(window).on("scroll", function (event) {
     var scrollValue = $(window).scrollTop();
     if (scrollValue > 320) {
-        $('.navbar').addClass('affix');
+        $(".navbar").addClass("affix");
         $(".navbar").addClass("animated fadeIn")
     } else {
-        $('.navbar').removeClass('affix');
+        $(".navbar").removeClass("affix");
     }
 });
