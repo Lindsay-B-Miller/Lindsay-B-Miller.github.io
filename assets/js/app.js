b53797fce6c252855f2df40ddb3392744bea5b2c
Lindsay <lindsay.lbm@gmail.com>
2019-11-11 18:35:20 -0600

    made navbar turn gray when scroll down

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
-/* affix the navbar after scroll below header */
-$('nav').affix({
-    offset: {
-        top: $('#services').offset().top
+// /* affix the navbar after scroll below header */
+// $('#nav').affix({
+//     offset: {
+//         top: $('#services').offset().top
+//     }
+// });
+$(window).on('scroll', function (event) {
+    var scrollValue = $(window).scrollTop();
+    if (scrollValue > 320) {
+        $('.navbar').addClass('affix');
+    } else {
+        $('.navbar').removeClass('affix');
     }
-});	
+});
