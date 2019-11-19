c8c6403ba95126764d6f25ed49880048ac0cb79e
Lindsay <lindsay.lbm@gmail.com>
2019-11-18 19:12:37 -0600

    working portfolio section on index

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,11 @@ $(window).on("scroll", function (event) {
     if (scrollValue > 320) {
         $(".navbar").addClass("affix");
         $(".navbar").addClass("animated fadeIn")
+        $(".portfolioImages").addClass("animated fadeInDown")
+        $(".portfolioImages").show()
     } else {
         $(".navbar").removeClass("affix");
+        $(".portfolioImages").hide()
     }
 });
 
